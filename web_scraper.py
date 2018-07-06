b22c85a49c79f676500aaed8eecdad2b61ed3789
Nariddh Khean <nariddh97@gmail.com>
2018-07-06 16:21:10 +1000

    Renamed File and Fixed Indentation
    Renamed file and fixed Indentation for viewing in GitHub

--- web_scraper.py
+++ web_scraper.py
@@ -2,25 +2,22 @@ from google_images_download import google_images_download
 import sys, os
 
 # Variables
-file_format	 = "jpg"
+file_format = "jpg"
 scrape_limit = 100
 
 # Search Terms
 search_terms = ["apples", "oranges"]
-keywords	 = ",".join(search_terms)
+keywords = ",".join(search_terms)
 
 # Output Directory
-user_path  = os.path.expanduser("~")
+user_path = os.path.expanduser("~")
 output_dir = os.path.join(user_path, "Desktop", "data")
 
 # Instantiate Class
 response = google_images_download.googleimagesdownload()
 
 # Insert Variables into Arguments
-arguements = {"keywords": keywords,
-			  "limit": scrape_limit,
-			  "format": file_format,
-			  "output_directory": output_dir}
+arguments = {"keywords": keywords, "limit": scrape_limit, "format": file_format, "output_directory": output_dir}
 
 # Execute Web Scrape
-absolute_image_paths = response.download(arguements)
+response.download(arguements)
