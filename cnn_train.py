6ec0d84a604e20b1a7f68bd6304f69cac06dd4bf
Nariddh Khean <nariddh97@gmail.com>
2018-07-06 17:29:39 +1000

    Keras Implementation of a CNN Classifier

---
+++ cnn_train.py
@@ -0,0 +1,84 @@
+import config
+
+import tensorflow as tf
+from keras import layers
+from keras import backend
+from keras.models import Model
+from keras.optimizers import RMSprop
+
+import os, sys
+
+
+### CONFIGURE ###
+
+# Error Messages
+tf.logging.set_verbosity(tf.logging.ERROR)
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"
+
+# TensorFlow Backend
+tf_config = tf.ConfigProto(gpu_options = tf.GPUOptions(allow_growth = True))
+backend.set_session(tf.Session(config = tf_config))
+
+
+### DATASET ###
+
+# NOTE: web_scraper.py currently scrapes images into folders titled with their labels.
+# Figure out a way to move around image files into training and validation.
+
+# Directories
+base_dir = config.output_dir
+training_dir = os.path.join(base_dir, "training")
+validation_dir = os.path.join(base_dir, "validation")
+
+# Training Directories
+training_a_dir = os.path.join(training_dir, config.search_terms[0])
+training_b_dir = os.path.join(training_dir, config.search_terms[1])
+
+# Validation Directories
+validation_a_dir = os.path.join(validation_dir, config.search_terms[0])
+validation_b_dir = os.path.join(validation_dir, config.search_terms[1])
+
+# Training Image List
+train_a_fnames = os.listdir(validation_a_dir)
+train_b_fnames = os.listdir(validation_b_dir)
+
+
+### CNN MODEL ###
+
+# Input Layer
+# [150 x 150 images, and 3 colour channels]
+img_input = layers.Input(shape = (150, 150, 3))
+
+# Hidden Layer 1 (CONVOLUTIONAL)
+# [16 3 x 3 filters, with 2 x 2 max-pooling]
+x = layers.Conv2D(16, 3, activation = "relu")(img_input)
+x = layers.MaxPooling2D(2)(x)
+
+# Hidden Layer 2 (CONVOLUTIONAL)
+# [32 3 x 3 filters, with 2 x 2 max-pooling]
+x = layers.Conv2D(32, 3, activation = "relu")(x)
+x = layers.MaxPooling2D(2)(x)
+
+# Hidden Layer 3 (CONVOLUTIONAL)
+# [64 3 x 3 filters, with 2 x 2 max-pooling]
+x = layers.Conv2D(64, 3, activation = "relu")(x)
+x = layers.MaxPooling2D(2)(x)
+
+# Hidden Layer 4 (FULLY CONNECTED)
+# [512 neurons]
+x = layers.Flatten()(x)
+x = layers.Dense(512, activation = "relu")(x)
+
+# Output Layer
+output = layers.Dense(1, activation = "sigmoid")(x)
+
+# Create Model
+model = Model(img_input, output)
+
+# Optimiser
+model.compile(loss = "binary_crossentropy", optimizer = RMSprop(lr = 0.001), metrics = ["acc"])
+
+# Review Model
+model_summary = True
+if model_summary:
+	print(model.summary())
--- config.py
+++ config.py
@@ -8,13 +8,13 @@ search_terms = ["apples", "oranges"]
 
 # Output Directory
 user_path = os.path.expanduser("~")
-output_dir = os.path.join(user_path, "Desktop", "data1")
+output_dir = os.path.join(user_path, "Desktop", "data")
 
 # File Format
 file_format = "jpg"
 
 # Scrape Limit
-scrape_limit = 2
+scrape_limit = 100
 
 
 ### CNN HYPERPARAMETERS ###
