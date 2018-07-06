b140ef45974e13756f38bfdc6ab45c6aeacc9f9e
Nariddh Khean <nariddh97@gmail.com>
2018-07-06 17:08:11 +1000

    Added Config
    Added config.py and cnn_train.py and adjusted web_scraper.py for variables from config.py

---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+
+
+### WEB SCRAPING ###
+
+# Search Terms
+search_terms = ["apples", "oranges"]
+
+# Output Directory
+user_path = os.path.expanduser("~")
+output_dir = os.path.join(user_path, "Desktop", "data1")
+
+# File Format
+file_format = "jpg"
+
+# Scrape Limit
+scrape_limit = 2
+
+
+### CNN HYPERPARAMETERS ###
+
+# Start
