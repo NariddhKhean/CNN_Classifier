1017596541cf2b1ae82b46835a507ad878912bd2
Nariddh Khean <nariddh97@gmail.com>
2018-07-08 15:38:16 +1000

    Updated Chromedriver Path

--- config.py
+++ config.py
@@ -4,20 +4,23 @@ import os
 ### WEB SCRAPE ###
 
 # Search Terms
-search_terms_a = ["apples", "apple"]
-search_terms_b = ["oranges", "orange"]
+search_terms_a = ["apples"]
+search_terms_b = ["oranges"]
 
 # Output Directores
 user_path = os.path.expanduser("~")
-data_path = os.path.join(user_path, "Desktop", "data")
+data_path = os.path.join(user_path, "Desktop/data")
 output_dir_a = os.path.join(data_path, search_terms_a[0])
 output_dir_b = os.path.join(data_path, search_terms_b[0])
+
+# Chromedriver Path
+chromedriver_path = os.path.join(user_path, "AppData/Local/Chromedriver/chromedriver.exe")
 
 # File Format
 file_format = "jpg"
 
 # Scrape Limit
-scrape_limit = 20
+scrape_limit = 250
 
 
 ### DATASET DIVISION ###
@@ -29,5 +32,5 @@ training_factor = 0.8
 ### CNN HYPERPARAMETERS ###
 
 # Training
-batch_size = 3
-epochs = 10
+batch_size = 10
+epochs = 250
