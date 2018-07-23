5492d6d82752b3afa576407e7ece089714fcf7aa
Nariddh Khean <nariddh97@gmail.com>
2018-07-23 11:26:22 +1000

    Fixed Indentation in model_predict.py

--- model_predict.py
+++ model_predict.py
@@ -19,28 +19,28 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 
 def predict(url):
 
-	# Load Trained Model
-	model = load_model(os.path.join(config.model_dir, config.model_name))
+    # Load Trained Model
+    model = load_model(os.path.join(config.model_dir, config.model_name))
 
-	# Request for Image from URL
-	img = Image.open(urllib.request.urlopen(url))
+    # Request for Image from URL
+    img = Image.open(urllib.request.urlopen(url))
 
-	# Resize, Scale, and Reshape for Input
-	img = img.resize((config.target_size, config.target_size))
-	img = np.array(img)
-	img = img / 255
-	img = np.reshape(img, (1, config.target_size, config.target_size, 3))
+    # Resize, Scale, and Reshape for Input
+    img = img.resize((config.target_size, config.target_size))
+    img = np.array(img)
+    img = img / 255
+    img = np.reshape(img, (1, config.target_size, config.target_size, 3))
 
-	# Predict
-	prediction = model.predict(img)[0][0]
+    # Predict
+    prediction = model.predict(img)[0][0]
 
-	# Output
-	if prediction < 0.5:
-		certainty = 1 - prediction
-		print("\nPrediction: '{}' with {:.3f}% certainty.".format(config.search_terms_a[0], certainty * 100))
-	else:
-		print("\nPrediction: '{}' with {:.3f}% certainty.".format(config.search_terms_b[0], prediction * 100))
+    # Output
+    if prediction < 0.5:
+        certainty = 1 - prediction
+        print("\nPrediction: '{}' with {:.3f}% certainty.".format(config.search_terms_a[0], certainty * 100))
+    else:
+        print("\nPrediction: '{}' with {:.3f}% certainty.".format(config.search_terms_b[0], prediction * 100))
 
 
 if __name__ == "__main__":
-	predict(sys.argv[1])
+    predict(sys.argv[1])
